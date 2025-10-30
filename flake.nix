7882bd9a5e23c3eac54c4f98b23cb9894ad323fc
Free-Rat <lawicki02@gmail.com>
2025-10-30 10:47:07 +0100

    nix devShell for project

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake with shell for LagBoards Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      shellHook = ''
+        alias efbo="ergogen . && open output/pcbs/lagbord.kicad_pcb"
+      '';
+
+      packages = with pkgs; [
+        ergogen
+        git
+      ];
+    };
+  };
+}
